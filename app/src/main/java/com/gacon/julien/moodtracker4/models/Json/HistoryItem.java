2e74d978ce4a9358a3bb4ca58f66d77ed88aea08
Julien Gacon <gaconjulien@gmail.com>
2018-09-14 17:15:49 +0200

    Add PieChart

--- app/src/main/java/com/gacon/julien/moodtracker4/models/Json/HistoryItem.java
+++ app/src/main/java/com/gacon/julien/moodtracker4/models/Json/HistoryItem.java
@@ -16,21 +16,29 @@ public class HistoryItem {
     private String mText2; // comment
     @SerializedName("Image color")
     private int mImageColor; // image color
+    @SerializedName("Mood Index")
+    private int mCurrentMood;
     @SerializedName("height")
     private int mHeight;
     @SerializedName("Width")
     private int mWidth;
 
     // HistoryItem constructor
-    public HistoryItem(String text1, String text2, int imageColor, int height, int width) {
+    public HistoryItem(String text1, String text2, int imageColor, int currentMood, int height, int width) {
         mText1 = text1; // date
         mText2 = text2; // comment
         mImageColor = imageColor; // image color
+        mCurrentMood = currentMood;
         mHeight = height; // height relative layout
         mWidth = width; // width relative layout
     } // end of HistoryItem constructor
 
     // Getters
+
+
+    public int getCurrentMood() {
+        return mCurrentMood;
+    }
 
     public int getHeight() {
         return mHeight;
